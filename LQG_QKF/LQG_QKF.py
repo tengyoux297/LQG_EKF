fixed bugs in lqe alg; now qkf has lower mse than ekf; variance/noise still unsure

LQG_QKF/LQG_QKF.py
@@ -76,9 +76,13 @@ class LQG:
         self.W = self.F.get_W()
         
         # augmented state settings
-        z0, _, _  = self.F.aug_state()       # 0 if x0 = 0
-        self.Z_est = z0.astype(np.float64)
-        self.Pz_est = np.eye(self.n + self.n**2) * small_value # estimation error covariance matrix
+        mu_tilde_u = (np.eye(n+n**2) - self.F.get_A_tilde()).T @ self.F.mu_tilde() # shape (n+n^2, 1)
+        self.Z_est = mu_tilde_u # initialize estimated augmented state vector
+        I = np.eye(n**2 * (n+1)**2) # shape (n^2(n+1)^2, n^2(n+1)^2)
+        Phi_tilde = self.F.get_A_tilde() # shape (n+n^2, n+n^2)
+        Sigma_tilde = self.F.aug_process_noise_covar() # shape (n+n^2, n+n^2)
+        vec_sigma_tilde_u = (I - np.kron(Phi_tilde, Phi_tilde)) @ Vec(Sigma_tilde) # shape (n^2(n+1)^2, 1)
+        self.Pz_est = invVec(vec_sigma_tilde_u) # estimation error covariance matrix
         
         # horizon
         self.H = H
@@ -139,88 +143,78 @@ class LQG:
         self.F.set_control(u_new) # update control input vector
         return 
     
-    
+    def update_lqr_orig(self, goal_state):
+        # LQR update only with original state, no augmented state
+        P_lqr = scipy.linalg.solve_discrete_are(self.A, self.B, self.Q[:self.n, :self.n], self.R)  # P is the fixed-point
+        feedback_gain = -np.linalg.pinv(self.R + self.B.T @ P_lqr @ self.B) @ self.B.T @ P_lqr @ self.A
+        u_new = feedback_gain @ (goal_state - self.x_hat)  # control input
+        self.F.set_control(u_new)
+        return
+        
     def update_lqr(self):
         goal_state = self.get_goal_state()
         if self.filter_type == 'ekf' or self.filter_type == 'kf':
-            # LQR update only with original state, no augmented state
-            P_lqr = scipy.linalg.solve_discrete_are(self.A, self.B, self.Q[:self.n, :self.n], self.R)  # P is the fixed-point
-            feedback_gain = -np.linalg.pinv(self.R + self.B.T @ P_lqr @ self.B) @ self.B.T @ P_lqr @ self.A
-            u_new = feedback_gain @ (goal_state - self.x_hat)  # control input
-            self.F.set_control(u_new)
-            return
+            self.update_lqr_orig(goal_state)
         elif self.filter_type == 'qkf':
-            self.update_lqr_qkf(goal_state, True)
+            self.update_lqr_orig(goal_state)
+            # self.update_lqr_qkf(goal_state, True)
         else:
             raise ValueError("Invalid filter type. Choose 'qkf', 'ekf', or 'kf'.")
         return
         
     
     def update_lqe_qkf(self):
-        Phi  = self.F.get_A_tilde()
+        Phi_tilde  = self.F.get_A_tilde()
         Sigma_tilde = self.F.aug_process_noise_covar()
         mu_tilde = self.F.mu_tilde()
         
         # state prediction     Z_{t|t‑1} ,  P⁽ᶻ⁾_{t|t‑1}
-        Z_pred = Phi @ self.Z_est + mu_tilde
-        Pz_pred = Phi @ self.Pz_est @ Phi.T + Sigma_tilde
+        Z_pred = Phi_tilde @ self.Z_est + mu_tilde
+        Pz_pred = Phi_tilde @ self.Pz_est @ Phi_tilde.T + Sigma_tilde
 
         # measurement prediction Y_{t|t‑1} , innovation cov  M
         measA = self.sensor.get_measA() # shape (m, 1)
         measB_tilde = self.sensor.get_aug_measB() # shape (m, n+n^2)
         Y_pred = measA + measB_tilde @ Z_pred # shape (m, n+n^2)
-        estM = measB_tilde @ Pz_pred @ measB_tilde.T + self.V
+        M = measB_tilde @ Pz_pred @ measB_tilde.T + self.V
 
         # Kalman gain          Kₜ
-        K = Pz_pred @ measB_tilde.T @ np.linalg.inv(estM)
+        K = Pz_pred @ measB_tilde.T @ np.linalg.inv(M)
 
         # state update         Z_{t|t} ,  P⁽ᶻ⁾_{t|t}
-        z, _, _ = self.F.aug_state()
-        Y_meas = self.sensor.aug_measure(z)
+        Z, _, _ = self.F.aug_state()
+        Y_meas = self.sensor.aug_measure(Z)
         innovation = Y_meas - Y_pred
         self.Z_est = Z_pred + K @ innovation
+        Pz_1 = Pz_pred - K @ M @ K.T
         
-        Pz_1 = Pz_pred - K @ estM @ K.T
         self.Pz_est = Pz_1
-        # keep the classical x̂ part handy for the LQR
-        n = self.n
-        self.x_hat = self.Z_est[:n, :]
+        self.x_hat = self.Z_est[:self.n, :]
         return K
     
     def update_lqe_ekf(self):
-        # Jacobian of the measurement function
-        m = self.sensor.C.shape[0]
-        temp_term = [None] * m
-        for i in range(m):
-            temp_term[i] = 2 * self.sensor.M[i] @ self.F.get_current_state()
-        temp_term = np.array(temp_term)
-        C_tilde = self.sensor.C + temp_term.squeeze()
+        mu = self.F.B @ self.F.u
+        Phi = self.F.A
+        Sigma = self.F.W
         
-        # priori estimate 
-        x_hat_pri = self.A @ self.x_hat + self.B @ self.F.get_current_control()   
+        # state prediction
+        X_pred = mu + Phi @ self.x_hat
+        P_pred = Phi @ self.P_est @ Phi.T + Sigma
         
-        # P_k-1
-        p0 = self.A @ self.P_est @ self.A.T + self.W
+        # measurement prediction
+        Y_pred = self.sensor.measure_pred(X_pred)
+        g = self.sensor.g(X_pred)
+        M = g @ P_pred @ g.T + self.sensor.V
         
-        # kalman gain
-            # K = P- @ C^T @ inv(C @ P- @ C^T + V)
-        kalman_gain =(p0 @ C_tilde.T @ np.linalg.pinv(C_tilde @ p0 @ C_tilde.T + self.V))
-        self.kalman_gain = (kalman_gain)
+        # gain
+        K = P_pred @ g.T @ np.linalg.inv(M)
         
-        # measurement
-        y = self.sensor.measure(self.F.get_current_state())
-        
-        # innovation
-        innov = y - self.sensor.measure_pred(x_hat_pri)
-        
-        # posterior estimate
-        x_hat_post = x_hat_pri + kalman_gain @ innov
-        self.x_hat = (x_hat_post)
-        
-        # P_k - Propagation of the estimation error covariance matrix
-        p1 = (np.eye(self.n) - kalman_gain @ C_tilde) @ p0
-        self.P_est = (p1)
-        return kalman_gain
+        # state update
+        Y_meas = self.sensor.measure(self.F.get_current_state())
+        innov = Y_meas - Y_pred
+        self.x_hat = X_pred + K @ innov
+        self.P_est = P_pred - K @ M @ K.T
+        return K
     
     def update_lqe_kf(self):
         C = self.sensor.C
@@ -246,8 +240,7 @@ class LQG:
         self.x_hat = (x_hat_post)
         
         # P_k - Propagation of the estimation error covariance matrix
-        p1 = (np.eye(self.n) - kalman_gain @ C) @ p0
-        self.P_est = (p1)
+        self.P_est = (np.eye(self.n) - kalman_gain @ C) @ p0
         return kalman_gain
     
     def update_lqe(self):
@@ -260,9 +253,7 @@ class LQG:
         else:
             raise ValueError("Invalid filter type. Choose 'qkf', 'ekf', or 'kf'.")
         t = self.F.t
-        if t % 100 == 0:
-            print(f't={t:4d}',
-                f'‖K_{self.filter_type}‖₂=', np.linalg.norm(K),)
+        # print(f'  t={t:4d}', f'‖K_{self.filter_type}‖₂=', np.linalg.norm(K),) if t % 100 == 0 else None
         return
 
     
@@ -293,19 +284,19 @@ def main():
     p = 3
     m = 2
     
-    W = generate_random_symmetric_matrix(n, scale=1e-2)
-    A = np.random.randn(n, n) * 0.8 * 1e-2
-    B = np.random.randn(n, p) * 0.5 * 1e-2
+    W = generate_random_symmetric_matrix(n, scale=1e-1)
+    A = np.random.randn(n, n) * 1e-1 
+    B = np.random.randn(n, p) * 1e-1
     
     
     C = np.random.randn(m, n)
     
     M = []
     for i in range(m):
-        M.append(generate_random_symmetric_matrix(n, scale=1e1))
+        M.append(generate_random_symmetric_matrix(n, scale=1e2))
     M = np.array(M)
     
-    V = generate_random_symmetric_matrix(m, scale=1e-2)
+    V = generate_random_symmetric_matrix(m, scale=1e-1)
     
     # Q, R must be symmetric positive definite matrices
     Q = generate_random_symmetric_matrix(n+n**2, scale=1.0)

LQG_QKF/stateDynamics.py
@@ -3,9 +3,25 @@ import numpy as np
 import scipy.linalg as la
 
 def Vec(X):
-    """Vectorize a matrix X (column-major ordering)."""
+    """
+    Vectorize a matrix X (column-major ordering).
+    for example:
+      input shape: (a, b)
+      output shape: (a*b, 1)
+    """
     return X.reshape(-1, 1, order='F') 
 
+def invVec(X):
+    """
+    Inverse vectorization of a matrix X (column-major ordering).
+    for example:
+      input shape: (a*b, 1)
+      output shape: (a, b)
+    """
+    n = int(np.sqrt(X.shape[0]))
+    return X.reshape(n, n, order='F') # reshape to (n,n) matrix
+  
+  
 class StateDynamics(object):
   
   def __init__(self, n, p, W, A, B):
@@ -18,14 +34,13 @@ class StateDynamics(object):
     assert W.shape[0] == n, "W must be a square matrix of size n x n"
     assert W.shape[1] == n, "W must be a square matrix of size n x n"
     self.W = W # covariance matrix for process noise w
-    self.w = np.zeros((n,1))
     self.n = n # state size
     self.p = p # control input size
     self.A = A.astype(np.float64) # state transition matrix
     self.B = B.astype(np.float64) # control input matrix
     self.t = 0 # time step
     self.trajectory = [] # trajectory of the system
-    self.trajectory.append([self.x, self.u, self.w]) # append initial state and control input to trajectory
+    self.trajectory.append([self.x, self.u]) # append initial state and control input to trajectory
   
   def get_state_size(self):
     return self.n
@@ -38,12 +53,6 @@ class StateDynamics(object):
     covariance matrix for process noise w
     '''
     return self.W
-  
-  def get_w(self):
-    '''
-    process noise
-    '''
-    return self.w
   
   def get_A(self):
     '''
@@ -85,17 +94,18 @@ class StateDynamics(object):
     '''
     Forward kinematics of the system.
     '''
-    self.w = np.random.multivariate_normal(np.zeros(self.W.shape[0]), self.W).reshape(-1, 1) # process noise vector, shape (n,1)
-    x1 = self.A @ self.x + self.B @ self.u + self.w # shape (n,1)
+    omega = np.linalg.cholesky(self.W) 
+    noise = omega @ np.random.randn(self.n, 1) # shape (n,1)
+    x1 = self.A @ self.x + self.B @ self.u + noise # shape (n,1)
     self.x = x1 # update state vector
     self.t += 1
-    self.trajectory.append([self.x, self.u, self.w]) # append current state and control input to trajectory
+    self.trajectory.append([self.x, self.u]) # append current state and control input to trajectory
     return self.t
   
 # augmented system
   def aug_state(self):
     '''
-    Augmented state vector
+    get current augmented state vector
     '''
     x = self.x # shape (n,1)
     z1 = x # shape (n,1)
@@ -104,51 +114,57 @@ class StateDynamics(object):
     return z, z1, z2
   
   def mu_tilde(self): # mu_tilde
-    Bu = self.B @ self.u # shape (n,1)
-    term1 = Bu # shape (n,1)
-    term2 = Vec(Bu @ Bu.T + self.W) # shape (n^2, 1) 
-    u_tilde = np.vstack((term1, term2)) # shape (n+n^2, 1)
-    return u_tilde # shape (n+n^2, 1)
+    mu = self.B @ self.u # "mu" term, shape (n,1)
+    Sigma = self.W # shape (n,n)
+    term1 = mu # shape (n,1)
+    term2 = Vec(mu @ mu.T + Sigma) # shape (n^2, 1) 
+    mu_tilde = np.vstack((term1, term2)) # shape (n+n^2, 1)
+    return mu_tilde # shape (n+n^2, 1)
   
   def aug_process_noise_covar(self):
     n = self.n # state size
     Sigma = self.W # shape (n,n)
     I_n = np.eye(n) # shape (n,n)
+    I_n2 = np.eye(n**2) # shape (n^2,n^2)
     Mu = self.B @ self.u # shape (n,1)
+    Phi = self.A # shape (n,n)
+    X = self.x # shape (n,1)
     # print(f'n: {n}, Mu: {Mu.shape}, Sigma: {Sigma.shape}')
-    Gamma = np.kron(I_n, (Mu + self.A @ self.x)) + np.kron((Mu + self.A @ self.x), I_n) # shape (n^2,n)
+    Gamma = np.kron(I_n, (Mu + Phi @ X)) + np.kron((Mu + Phi @ X), I_n) # shape (n^2,n)
+    
     Lambda = np.zeros((n**2, n**2))
     for i in range(n):
         for j in range(n):
-            row = i * n + j   # vec index of (i, j)
-            col = j * n + i   # vec index of (j, i)
-            Lambda[row, col] = 1   # single 1 per row/col
+            e_i = np.zeros((n,1))
+            e_i[i] = 1
+            e_j = np.zeros((n,1))
+            e_j[j] = 1
+            Lambda += np.kron(e_i @ e_j.T, e_j @ e_i.T) # shape (n^2,n^2)   
+            
     Sigma11 = Sigma # shape (n,n)
     # print(f'Sigma: {Sigma.shape}, Gamma: {Gamma.shape}, Lambda: {Lambda.shape}')
     Sigma12 = Sigma @ Gamma.T # shape (n,n^2)
     Sigma21 = Gamma @ Sigma # shape (n^2,n)
-    Sigma22 = Gamma @ Sigma @ Gamma.T + (np.eye(n**2) + Lambda) @ np.kron(Sigma, Sigma) # shape (n^2,n^2)
+    Sigma22 = Gamma @ Sigma @ Gamma.T + (I_n2 + Lambda) @ np.kron(Sigma, Sigma) # shape (n^2,n^2)
     
-    Sigma_tilde = np.zeros((n+n**2, n+n**2)) # shape (n+n^2,n+n^2)
+    Sigma_tilde = np.zeros((n+n**2, n+n**2), dtype=np.float64) # shape (n+n^2,n+n^2
     Sigma_tilde[:n, :n] = Sigma11
     Sigma_tilde[:n, n:] = Sigma12
     Sigma_tilde[n:, :n] = Sigma21
     Sigma_tilde[n:, n:] = Sigma22
-    # scale_z2 = 0.01      # try 0.01 … 0.0001
-    # Sigma_tilde[n:, n:] *= scale_z2
-    return Sigma_tilde.astype(np.float64) # shape (n+n^2,n+n^2)
+    return Sigma_tilde # shape (n+n^2,n+n^2)
   
   def get_A_tilde(self):
     '''
-    Augmented state transition matrix
+    Augmented state transition matrix; "Phi" term in function X = mu + Phi X + Omega epsilon
     '''
     n = self.n # state size
-    A = self.A # state transition matrix, shape (n,n)
-    Bu = self.B @ self.u # shape (n,1)
-    A11 = A # shape (n,n)
+    Phi = self.A # state transition matrix, shape (n,n)
+    mu = self.B @ self.u # shape (n,1)
+    A11 = Phi # shape (n,n)
     A12 = np.zeros((n, n**2)) # shape (n,n^2)
-    A21 = np.kron(Bu, A) + np.kron(A, Bu) # shape (n^2,n)
-    A22 = np.kron(A, A) # shape (n^2,n^2)
+    A21 = np.kron(mu, Phi) + np.kron(Phi, mu) # shape (n^2,n)
+    A22 = np.kron(Phi, Phi) # shape (n^2,n^2)
     A_tilde = np.zeros((n+n**2, n+n**2)) # shape (n+n^2,n+n^2)
     A_tilde[:n, :n] = A11
     A_tilde[:n, n:] = A12
@@ -158,31 +174,17 @@ class StateDynamics(object):
 
   def get_B_tilde(self):
       """
-      Return B_tilde = [ B ;
-                        (μ⊗I + I⊗μ) B ]
-      where μ = B u.
+      Expected to be derived by d mu_tilde / d u
       """
       B  = self.B                               # (n, p)
-      μ  = B @ self.u                           # (n, 1)
+      u = self.u                               # (p, 1)
+      mu  = B @ self.u                           # (n, 1)
       n, p = self.n, self.p
-
-      B1 = B                                    # (n, p)
-      kron_sum = (np.kron(μ, np.eye(n)) +       # (n², n)
-                  np.kron(np.eye(n), μ))        # (n², n)
-      B2 = kron_sum @ B                         # (n², p)
-
-      return np.vstack((B1, B2)).astype(np.float64)   # (n + n², p)
-  
-  # def get_B_tilde(self): # B_tilde
-  #   B = self.B # shape (n,p)
-  #   u = self.u # shape (p,1)
-  #   n, p = B.shape[0], B.shape[1] # state size, control input size
-  #   # top block
-  #   B_tilde_1 = B                        # (n, p)
-  #   kron_sum = np.kron(u, np.eye(p)) + np.kron(np.eye(p), u)   # (p², p)
-  #   B_tilde_2 = np.kron(B,B) @ kron_sum                             # (n^2, p)
-  #   B_tilde = np.vstack([B_tilde_1, B_tilde_2]) # (n+n^2, p)
-  #   return B_tilde.astype(np.float64) # shape (n+n^2,p)
+      I_n = np.eye(n)                        # (n, n)
+      I_p = np.eye(p)                        # (p, p)
+      term1 = B                                    # (n, p)
+      term2 = np.kron(B, B) @ (np.kron(I_p, u) + np.kron(u, I_p)) # (n², p)
+      return np.vstack((term1, term2), dtype=np.float64)   # (n + n², p)
 
 class sensor(object):
   def __init__(self, C, M, V):
@@ -190,14 +192,13 @@ class sensor(object):
     Initializes the class
     """
     # M shape - (m, n, n) where n is state size
-    assert C.shape[0] == M.shape[0], "C and M must have the same length"
+    assert C.shape[0] == M.shape[0], "C and M must have the same length m"
     self.m = M.shape[0] # number of measurements
     self.n = C.shape[1]
     
     self.M = M.astype(np.float64) # covariance matrix for process noise w
     self.V = V.astype(np.float64) # covariance matrix for measurement noise v
     self.C = C.astype(np.float64) # measurement matrix, shape (m, n)
-    self.v = np.zeros((self.m,1)) # measurement noise vector
     
   def get_V(self):
     '''
@@ -207,14 +208,14 @@ class sensor(object):
   
   def get_measA(self):
     '''
-    Augmented measurement matrix 1
+    measurement matrix 1; "A" term in Y = A + Bx + Σ e X.T C X + Dv
     Actualy, this matrix does not exist in our case, but we need it for the QKF, so we define it as a zero matrix.
     '''
     return np.zeros((self.m, 1)) # shape (m,1)
 
   def get_aug_measB(self):
     '''
-    Augmented measurement matrix 2
+    Augmented matrix of measurement matrix 2; B_tilde from "B" term in Y = A + Bx + Σ e X.T C X + Dv
     '''
     if self.M.ndim != 3:
         raise ValueError("M_stack must be (m, n, n) or list of (n, n) matrices")
@@ -224,27 +225,56 @@ class sensor(object):
         raise ValueError("Inconsistent shapes between C and M_stack")
 
     # build the right-hand block: each row i is vec(M[i].T)
-    M_vec_block = self.M.transpose(0, 2, 1).reshape(m, n * n)  # (m, n²)
+    right_term = np.zeros((m, n**2)) # shape (m, n^2)
+    for i in range(m):
+        right_term[i] = Vec(self.M[i].T).squeeze() # shape (n^2,1) -> shape (n^2)
 
     # horizontal concatenation
-    C_tilde = np.hstack((self.C, M_vec_block)) # shape (m, n+n^2)
-    return C_tilde    
+    B_tilde = np.hstack((self.C, right_term)) # shape (m, n+n^2)
+    # print (f'B_tilde shape: {B_tilde.shape}')
+    return B_tilde    
   
   def measure(self, x):
-    self.v = np.random.multivariate_normal(np.zeros(self.V.shape[0]), self.V).reshape(-1,1)
-
+    '''
+    Measurement function
+    '''
     term1 = self.C @ x
-    term2 = np.array([x.T @ self.M[i] @ x for i in range(self.m)]).reshape(-1,1)
-    return term1 + term2 + self.v
+    term2 = np.zeros((self.m, 1))
+    for i in range(self.m):
+        e = np.zeros((self.m, 1))
+        e[i] = 1
+        term2 += e @ x.T @ self.M[i] @ x
+    D = np.linalg.cholesky(self.V) 
+    term3 = D @ np.random.randn(self.m, 1)
+    return term1 + term2 + term3
   
   def measure_pred(self, x_pred):
+    '''
+    Measurement function for predicted state
+    '''
     term1 = self.C @ x_pred
-    term2 = np.array([x_pred.T @ self.M[i] @ x_pred for i in range(self.m)]).reshape(-1,1)
+    term2 = np.zeros((self.m, 1))
+    for i in range(self.m):
+        e = np.zeros((self.m, 1))
+        e[i] = 1
+        term2 += e @ x_pred.T @ self.M[i] @ x_pred
     return term1 + term2
   
+  def g(self, x):
+    term1 = self.C # B term in Y = A + Bx + Σ e X.T C X + Dv
+    term2 = np.zeros((self.m, self.n))
+    for i in range(self.m):
+        e = np.zeros((self.m, 1))
+        e[i] = 1
+        term2 += e @ x.T @ self.M[i]
+    return term1 + 2 * term2 # shape (m,n)
+  
   def aug_measure(self, z):
-    self.v = np.random.multivariate_normal(np.zeros(self.V.shape[0]), self.V).reshape(-1, 1)
-    term1 = self.get_measA() # shape (m,1)
+    '''
+    Measurement function for augmented state
+    '''
+    term1 = self.get_measA() # shape (m,1), which is a zero vector
     term2 = self.get_aug_measB() @ z # shape (m,1)
-    term3 = self.v # shape (m,1)
+    D = np.linalg.cholesky(self.V) 
+    term3 = D @ np.random.randn(self.m, 1) # shape (m,1)
     return term1 + term2 + term3 # shape (m,1)